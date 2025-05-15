5aa1571c2b87a88a67def52ac3925d0650f81c20
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-05-15 08:34:24 +0530

    databinding

--- src/app/components/databinding/databinding.component.ts
+++ src/app/components/databinding/databinding.component.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   templateUrl: './databinding.component.html',
+  styles:['h3{color:purple}'],
   styleUrl: './databinding.component.css'
 })
 export class DatabindingComponent {
@@ -15,8 +16,17 @@ export class DatabindingComponent {
   flag: boolean = true;
   msg: string = '';
   selectedState: string = 'Karnataka';
+  colspanValue: number = 2;
+
+  num1: number = 0;
+  num2: number = 0;
 
   f1() {
     alert('I am f1...')
+  }
+
+  addResult: number = 0;
+  addition(num3: any, num4: any) {
+    this.addResult = +num3 + +num4;
   }
 }
