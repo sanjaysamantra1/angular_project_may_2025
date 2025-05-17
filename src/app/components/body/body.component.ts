68f25991f1827ccb794b0dc020cb8818b8bfc216
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-05-17 08:18:48 +0530

    directives

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,12 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { DatabindingComponent } from '../databinding/databinding.component';
 import { DirectivesComponent } from '../directives/directives.component';
+import { UsersComponent } from '../users/users.component';
 
 @Component({
   selector: 'app-body',
   imports: [
     // DatabindingComponent,
-    DirectivesComponent
+    // DirectivesComponent
+    UsersComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
