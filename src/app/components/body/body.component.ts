9cb0d928dddbc8d14ad35dcb989848b6e04de4b8
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-05-19 08:45:00 +0530

    products

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,13 +2,17 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { DatabindingComponent } from '../databinding/databinding.component';
 import { DirectivesComponent } from '../directives/directives.component';
 import { UsersComponent } from '../users/users.component';
+import { MyModalComponent } from '../my-modal/my-modal.component';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-body',
   imports: [
     // DatabindingComponent,
-    DirectivesComponent
+    // DirectivesComponent
     // UsersComponent
+    // MyModalComponent
+    ProductsComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
